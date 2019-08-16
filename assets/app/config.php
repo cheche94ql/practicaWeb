b85eee11c8bbfa2a72d35dfcf27c3cbe35173479
Cristopher Quiros Lopez <cristopherquiros0@gmail.com>
2019-08-15 19:18:59 -0600

    Cambio en comentario

--- assets/app/config.php
+++ assets/app/config.php
@@ -3,6 +3,7 @@ $nombre_servidor= 'localhost';
 $nombre_usuario= 'root';
 $password= '';
 $nombre_base_datos ='blog';
-//informacion para conectar con la base de datos 
+//informacion para conectar con la base de datos.
+//prueba branch
 
 ?>
